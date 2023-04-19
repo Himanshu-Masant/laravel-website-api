bda88bd2b717454e9d671e3a086acbbeb6883fba
Himanshu Masant <himanshumasant@uxdlabtech.com>
2023-04-19 18:04:24 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("It's Working");
+});
+
+app.post("/sample", (req, res) => {
+  const obj = {
+    is_main_product_valid: true,
+    main_product_registered: false,
+    invalid_modules: [],
+    modules_not_registered: [],
+    multiple_registration: false,
+    multiple_registration_modules: [],
+  };
+
+  res.send(obj);
+});
+
+app.listen(port, () => {
+  console.log(`Active on port http://localhost:${port}`);
+});
